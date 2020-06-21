3f0e6533d0688598774a963f6f95ba47dd914cf1
Ashwin Godbole <dev.godboleashwin@gmail.com>
2020-06-21 22:01:18 +0530

    Update and rename TemplateState.kt to ProductState.kt

---
+++ contracts/src/main/kotlin/supplyChainManagement/states/ProductState.kt
@@ -0,0 +1,24 @@
+package supplyChainManagement.states
+
+import net.corda.core.contracts.BelongsToContract
+import net.corda.core.contracts.LinearState
+import net.corda.core.contracts.UniqueIdentifier
+import net.corda.core.identity.Party
+import supplyChainManagement.Location
+import supplyChainManagement.ProductData
+import supplyChainManagement.contracts.ProductContract
+import java.time.LocalDateTime
+
+@BelongsToContract(ProductContract::class)
+data class ProductState (
+
+    val inputsFrom          : Party,                // Who supplied the raw materials to the manufacturer ?
+    val manufacturer        : Party,                // Who manufactured said product ?
+    val materialData        : ProductData,          // What product was manufactured ?
+    val ext_location        : Location,             // Where was the product manufactured ?
+    val extTimeDate         : LocalDateTime,        // When was the product manufactured ?
+    override val linearId   : UniqueIdentifier = UniqueIdentifier()
+
+) : LinearState {
+    override val participants: List<Party> get() = listOf(inputsFrom, manufacturer)
+}
