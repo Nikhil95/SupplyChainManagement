54b9597aea031cd1f4d80ee2c84e2ec20705c802
Ashwin Godbole <dev.godboleashwin@gmail.com>
2020-06-24 16:50:20 +0530

    Update ProductContract.kt

--- contracts/src/main/kotlin/supplyChainManagement/contracts/ProductContract.kt
+++ contracts/src/main/kotlin/supplyChainManagement/contracts/ProductContract.kt
@@ -4,7 +4,6 @@ import supplyChainManagement.states.ProductState
 import net.corda.core.contracts.*
 import net.corda.core.transactions.LedgerTransaction
 import supplyChainManagement.isValidUnit
-import supplyChainManagement.states.RawMaterialState
 
 class ProductContract : Contract {
     companion object {
@@ -16,14 +15,13 @@ class ProductContract : Contract {
         val inputs = tx.inputs
         val outputs = tx.outputs
         when (command.value) {
-            // Intermediate product will always be issued from a RawMaterialState.
             is Commands.IssueIP -> requireThat {
-                "Issue IP must have no inputs" using (inputs.isEmpty())
-                "Issue IP must have 1 output" using (outputs.size == 1)
+                "Issue IP must have no inputs"                            using (inputs.isEmpty())
+                "Issue IP must have 1 output"                             using (outputs.size == 1)
                 val outState = tx.outputStates[0] as ProductState
-                "only positive amounts of a product can be manufactured" using (outState.materialData.qty > 0)
-                "the quantity of the product manufactured must be given in valid units" using (isValidUnit(outState.materialData.units))
-                "Both participants of output state must sign the transaction" using (command.signers.toSet() == outState.participants.map { it.owningKey }.toSet())
+                "only positive amounts of a product can be manufactured"  using (outState.sentMaterialData.qty > 0)
+                "the quantity of the product manufactured must be given in valid units"     using (isValidUnit(outState.sentMaterialData.units))
+                "Both participants of output state must sign the transaction"               using (command.signers.toSet() == outState.participants.map { it.owningKey }.toSet())
             }
             is Commands.TransferIP -> requireThat {
                 val inState = tx.inputStates[0] as ProductState
@@ -32,9 +30,9 @@ class ProductContract : Contract {
                 "transfer IP must have its inputs as a ProductState"                        using (tx.inputStates[0] is ProductState)
                 "transfer IP must have 1 or more outputs"                                   using (outputs.isNotEmpty())
                 "product cannot be manufactured before inputs are received"                 using (outState.extTimeDate.isAfter(inState.extTimeDate))
-                "only positive amounts of a product can be manufactured"                    using (outState.materialData.qty > 0)
-                "the quantity of the product manufactured must be given in valid units"     using (isValidUnit(outState.materialData.units))
-                "Inputs for output state must be from manufacturer of input state"          using (inState.manufacturer.owningKey == outState.inputsFrom.owningKey)
+                "only positive amounts of a product can be manufactured"                    using (outState.sentMaterialData.qty > 0)
+                "the quantity of the product manufactured must be given in valid units"     using (isValidUnit(outState.sentMaterialData.units))
+                "Inputs for output state must be from manufacturer of input state"          using (inState.receiver.owningKey == outState.sender.owningKey)
 
                 "Everybody related to the txn (inputsFrom and manufacturer of input state and output state must sign the transaction" using
                     (command.signers.toSet() == (inState.participants.map { it.owningKey }.toSet() `union` outState.participants.map { it.owningKey }.toSet()))
